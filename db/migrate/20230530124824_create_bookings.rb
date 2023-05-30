fcccf01aed4091d9a4ed714d050e80ceca2f2031
Theresa Eberle <theresa.eberle@posteo.de>
2023-05-30 15:26:36 +0200

    created booking model with dependencies

---
+++ db/migrate/20230530124824_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :booking_date
+      t.references :skill, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
