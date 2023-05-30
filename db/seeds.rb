368ff88e3eab383f8a876b00d3fa6c237e22f9ae
Theresa Eberle <130083569+theeberle@users.noreply.github.com>
2023-05-30 15:22:51 +0200

    Update seeds.rb
    variables changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-users = User.create([{ first_name: "Bob" }, { last_name: "Ross" }, {username: "BobR" }, {email: "bob.ross@gmail.com" }, {role: "contractor" }])
-users = User.create([{ first_name: "Michael" }, { last_name: "Schumacher" }, {username: "MichaelS" }, {email: "michael.schumacher@gmail.com" }, {role: "rentor" }])
-users = User.create([{ first_name: "Agathe" }, { last_name: "Chopin" }, {username: "AgatheS" }, {email: "agathe.chopin@gmail.com" }, {role: "unicorn" }])
-users = User.create([{ first_name: "Elisa" }, { last_name: "Meyer" }, {username: "ElisaM" }, {email: "Elisa.Meyer@gmail.com" }, {role: "unicorn" }])
+bob = User.create([{ first_name: "Bob" }, { last_name: "Ross" }, {username: "BobR" }, {email: "bob.ross@gmail.com" }, {role: "contractor" }])
+michi = User.create([{ first_name: "Michael" }, { last_name: "Schumacher" }, {username: "MichaelS" }, {email: "michael.schumacher@gmail.com" }, {role: "rentor" }])
+agathe = User.create([{ first_name: "Agathe" }, { last_name: "Chopin" }, {username: "AgatheS" }, {email: "agathe.chopin@gmail.com" }, {role: "unicorn" }])
+elisa = User.create([{ first_name: "Elisa" }, { last_name: "Meyer" }, {username: "ElisaM" }, {email: "Elisa.Meyer@gmail.com" }, {role: "unicorn" }])
