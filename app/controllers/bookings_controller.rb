d57377132b85dacf0f4129d506a1e423cfa3deb9
Theresa Eberle <theresa.eberle@posteo.de>
2023-05-31 13:00:50 +0200

    saving controllerbookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,18 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    raise
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to profile_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:booking_date)
   end
 end
